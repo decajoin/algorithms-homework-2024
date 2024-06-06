84dc56b2ec9749ad905258bd28233af38355a735
machenike <machenike@gmail.com>
2024-06-06 12:26:20 +0800

    增加计时部分代码

--- HomeWork3/code/Code01KP.py
+++ HomeWork3/code/Code01KP.py
@@ -1,3 +1,5 @@
+import time
+
 # 读取数据集
 def read_data(filename):
     with open(filename, 'r') as file:
@@ -11,7 +13,7 @@ def greedy_knapsack(items, capacity):
     value_per_weight = [(item[0] / item[1], item[0], item[1]) for item in items]
     # 按单位价值降序排序
     value_per_weight.sort(reverse=True)
-    
+
     # 贪心选择物品放入背包
     knapsack = []
     total_value = 0
@@ -22,23 +24,30 @@ def greedy_knapsack(items, capacity):
             knapsack.append((value, weight))
             total_value += value
             remaining_capacity -= weight
-    
-    return knapsack, total_value, remaining_capacity
+
+    return knapsack, total_value
 
 if __name__ == "__main__":
     # 读取数据集
-    filename = "HomeWork3\code\dataset_01KP\large_scale\knapPI_1_100_1000_1"
+    filename = "HomeWork3\code\dataset_01KP\low-dimensional\\f8_l-d_kp_23_10000"
     items = read_data(filename)
 
     # 移除数据集中的第一行，并获取物品数量和背包容量
-    n, capacity = items.pop(0)  
-    
+    n, capacity = items.pop(0)
+
+    # 计算执行时间
+    start_time = time.time()
+
     # 使用贪心算法解决01背包问题
-    knapsack, total_value, remaining_capacity = greedy_knapsack(items, capacity)
-    
+    knapsack, total_value = greedy_knapsack(items, capacity)
+
+    # 计算执行时间
+    execution_time = time.time() - start_time
+
     # 打印结果
     print("使用数据集" + filename + "进行测试")
     print("从背包中选择的物品如下:")
     for item in knapsack:
         print("价值:", item[0], "重量:", item[1])
-    print("总价值:", total_value, "总重量:", capacity - remaining_capacity)
+    print("总价值:", total_value)
+    print("执行时间:", execution_time, "秒")
--- HomeWork3/code/CodeTSP.py
+++ HomeWork3/code/CodeTSP.py
@@ -24,12 +24,12 @@ def greedy_tsp(coords):
     num_cities = len(coords)
     visited = [False] * num_cities
     path = []
-    
+
     # 选择起始点
     current_city = coords[0]
     path.append(current_city[0])
     visited[0] = True
-    
+
     # 逐步选择下一个城市
     for _ in range(num_cities - 1):
         min_dist = float('inf')
@@ -44,20 +44,20 @@ def greedy_tsp(coords):
         path.append(nearest_city[0])
         current_city = nearest_city
         visited[current_city[0] - 1] = True
-        
+
     # 回到起点
     path.append(path[0])
-    
+
     return path
 
 if __name__ == "__main__":
     # 读取数据集
-    filename = "HomeWork3\code\dataset_TSP\mu1979.tsp"
+    filename = "HomeWork3\code\dataset_TSP\wi29.tsp"
     coords = read_data(filename)
-    
+
     # 使用贪心算法解决旅行商问题
     path = greedy_tsp(coords)
-    
+
     # 打印路径
     print("使用数据集" + filename + "进行测试")
     print("最佳路径:", path)
